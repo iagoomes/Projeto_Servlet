90da59d0b88bcfd90feadf48b21df5df85b9b2b3
iagoomes <iagoomes@outlook.com>
2023-01-04 19:12:20 -0300

    Refatoracao do servlet controller para filter controller

--- gerenciador/src/main/java/br/com/iagoomes/gerenciador/servlet/AutorizacaoFilter.java
+++ gerenciador/src/main/java/br/com/iagoomes/gerenciador/servlet/AutorizacaoFilter.java
@@ -7,13 +7,16 @@ import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.HttpFilter;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-
-//@WebFilter("/entrada")
+/**
+ * Servlet Filter implementation class AutorizacaoFilter
+ */
+@WebFilter("/entrada")
 public class AutorizacaoFilter extends HttpFilter implements Filter {
 	private static final long serialVersionUID = 1L;
 
